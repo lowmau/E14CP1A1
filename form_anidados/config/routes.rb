04af0e806b6404e842104a535b4bd6583c9b517c
lowmau <lowtjierivero@gmail.com>
2018-11-16 22:28:10 -0300

    CRUD anidado

---
+++ form_anidados/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+	resources :categories do
+  	resources :products
+  end
+	
+  #resources :categories
+  root to: 'categories#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
